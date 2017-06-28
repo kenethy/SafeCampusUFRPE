edde357c5f9d415ec1d9803e9e23da1afc2d0ed6
cesar <cesarmarmun@gmail.com>
2017-06-28 11:51:13 -0300

    Atualização de Layouts

--- app/src/main/java/com/bignerdranch/android/safecampus/SafeCampusActivity.java
+++ app/src/main/java/com/bignerdranch/android/safecampus/SafeCampusActivity.java
@@ -8,6 +8,8 @@ public class SafeCampusActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_safe_campus);
+        setContentView(R.layout.activity_relatorio_de_ocorrencias);
     }
+
+
 }
